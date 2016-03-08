a35563cf89bb593f80253b37ded1e6a842798d02
Mark Hansen <stonedonkey@gmail.com>
2016-03-08 16:15:52 -0500

    Basic game navigation working with JS feeding through WebForm.

--- MameLaunch/Main.cs
+++ MameLaunch/Main.cs
@@ -17,25 +17,46 @@ namespace MameLaunch
 {
     public partial class Main : Form
     {
-        private List<Game> _games;
+        //private List<Game> _games;
+        private bool _skipOnce = false;
         public Main()
         {
             InitializeComponent();
 
-            LoadGames();
+            //LoadGames();
 
             string curDir = Directory.GetCurrentDirectory();
             this.wb.Url = new Uri(String.Format("file:///{0}/resources/template-main.html", curDir));
             
             wb.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(wb_DocumentCompleted);
             wb.PreviewKeyDown += new PreviewKeyDownEventHandler(wb_PreviewKeyDown);
+           
         }
 
         private void wb_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
         {
+            // web browser control has a bug where this is called twice
+            if (_skipOnce)
+            {
+                _skipOnce = false;
+                return;
+            }
+
             // exit on ESC key
             if (e.KeyValue == 27)
                 Application.Exit();
+            else if (e.KeyValue== 38)
+            {
+                wb.Document.GetElementById("btn-up").InvokeMember("click");
+                _skipOnce = true;
+            }
+             else if (e.KeyValue== 40)
+            {
+                wb.Document.GetElementById("btn-down").InvokeMember("click");
+                 _skipOnce = true;
+            }
+            
+
         }
         private void wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
@@ -61,10 +82,10 @@ namespace MameLaunch
         }
         private void LoadGames()
         {
-            string curDir = Directory.GetCurrentDirectory();
+           // string curDir = Directory.GetCurrentDirectory();
 
-            string json = File.ReadAllText(curDir + "\\resources\\json\\games.json");
-            _games = JsonConvert.DeserializeObject<List<Game>>(json);
+            //string json = File.ReadAllText(curDir + "\\resources\\json\\games.json");
+           // _games = JsonConvert.DeserializeObject<List<Game>>(json);
 
         }
 
--- MameLaunch/Resources/js/main.js
+++ MameLaunch/Resources/js/main.js
@@ -1,9 +1,51 @@
-﻿$(function () {
+﻿var currentGame = 0;
+
+$(function () {
     LoadGames();
+
+    $("#btn-down").click(function () {
+
+        if (currentGame + 1 >= games.length)
+            currentGame = 0;
+        else
+            currentGame++;
+      
+        $("#gameslist").empty();
+        RenderGameList();
+        
+    });
+
+    $("#btn-up").click(function () {
+
+        if (currentGame - 1 < 0)
+            currentGame = games.length-1;
+        else
+            currentGame--;
+
+        $("#gameslist").empty();
+        RenderGameList();
+    });
+
 });
 
-function LoadGames()
-{
-      
+function LoadGames() {
+    RenderGameList();
+    SetGame(currentGame);
+}
+function RenderGameList() {
+    var counter = 0;
+    $.each(games, function (key, value) {
+        if (currentGame == counter)
+            $("#gameslist").append('<li class=selected>' + value.name + '</li>');
+        else
+            $("#gameslist").append('<li>' + value.name + '</li>');
+
+        counter++;
+
+    });
 }
 
+function SetGame(gameid) {
+    $("#players").text(games[gameid].players);
+    $("#publisher").text(games[gameid].publisher);
+}
