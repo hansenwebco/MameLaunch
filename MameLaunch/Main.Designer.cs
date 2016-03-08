5905d533de5814c251bb9c6fe4e9db99610bc7cf
Mark Hansen <stonedonkey@gmail.com>
2016-03-08 11:41:33 -0500

    Initial Commit

---
+++ MameLaunch/Main.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace MameLaunch
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.wb = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // wb
+            // 
+            this.wb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.wb.Location = new System.Drawing.Point(0, 0);
+            this.wb.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb.Name = "wb";
+            this.wb.ScrollBarsEnabled = false;
+            this.wb.Size = new System.Drawing.Size(284, 262);
+            this.wb.TabIndex = 0;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.wb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser wb;
+    }
+}
+
---
+++ MameLaunch/Main.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MameLaunch
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            string curDir = Directory.GetCurrentDirectory();
+
+            this.wb.Url = new Uri(String.Format("file:///{0}/resources/template-main.html", curDir));
+            wb.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(wb_DocumentCompleted);
+            wb.PreviewKeyDown += new PreviewKeyDownEventHandler(wb_PreviewKeyDown);
+        }
+        private void wb_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            // exit on ESC key
+            if (e.KeyValue == 27)
+                Application.Exit();
+        }
+        private void wb_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+
+            HtmlElement ele = wb.Document.GetElementById("launch");
+            if (ele != null)
+            {
+                ele.Click -= htmlDoc_Click;
+                ele.Click += new HtmlElementEventHandler(htmlDoc_Click);
+            }
+        }
+        private void htmlDoc_Click(object sender, HtmlElementEventArgs e)
+        {
+            HtmlElement elm = (HtmlElement)sender;
+
+            string mamePath = System.Configuration.ConfigurationManager.AppSettings["MamePath"].ToString();
+
+            ProcessStartInfo Mame = new ProcessStartInfo(mamePath + "mame.exe");
+            Mame.WorkingDirectory = "c:\\temp\\mame";
+            Mame.WindowStyle = ProcessWindowStyle.Hidden;
+            Mame.Arguments = "mspacman.zip";
+            Process.Start(Mame);
+        }
+    }
+}
